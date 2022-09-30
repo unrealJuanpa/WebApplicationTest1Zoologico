362be47f6d7dddf959711d224c9438975dd36605
unrealJuanpa <juanpacloud@gmail.com>
2022-09-29 20:09:33 -0600

    Base dbContext agregado

---
+++ Models/DbContextZoo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest1Zoologico.Models
+{
+    public class DbContextZoo:DbContext
+    {
+        public DbContextZoo():base("ConnZoo")
+        {
+
+        }
+    }
+}
