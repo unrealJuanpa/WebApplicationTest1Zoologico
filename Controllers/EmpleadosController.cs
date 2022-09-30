422b62a11eebf0ee9552f6fe4a4181aacc37984d
unrealJuanpa <juanpacloud@gmail.com>
2022-09-29 20:45:22 -0600

    CRUD empleados agregado

---
+++ Controllers/EmpleadosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest1Zoologico.Models;
+
+namespace WebApplicationTest1Zoologico.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private DbContextZoo db = new DbContextZoo();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            return View(db.Empleadoses.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleadoses.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Direccion,Telefono")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleadoses.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleadoses.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Direccion,Telefono")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleadoses.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleadoses.Find(id);
+            db.Empleadoses.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
